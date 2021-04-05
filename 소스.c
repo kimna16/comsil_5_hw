c062a7b1c26391bca925004c1413d18272daa7c2
nahyun <kimna16@naver.com>
2021-04-05 15:44:42 +0900

    add 소스.c file

---
+++ 소스.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int i;
+	int sum = 0;
+	for (i = 1; i <= 10; i++) {
+		sum += i;
+		printf("sum= %d\n", sum);
+	}
+	printf("sum은 1부터 %d까지의 합이다.\n",i);
+
+	return 0;
+}
